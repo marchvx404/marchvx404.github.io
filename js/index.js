31ae336bc250e5bb72edf1ac820d8c64d6eccbc8
Marchva <115968991+marchvx404@users.noreply.github.com>
2024-08-01 09:29:16 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'Hai bubi aku punya sesuatu!'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
@@ -10,6 +10,6 @@ for (let index = 0; index < text.length; index++) {
 
 const textElements = document.querySelectorAll('.title span');
 textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  const randomDelay = Math.random() * 1; // Menghasilkan delay acak antara 0 hingga 3 detik
   element.style.animationDelay = `${randomDelay}s`;
 });
